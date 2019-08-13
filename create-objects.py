93ca2746d069ba4ad3a10d115540bef54daa6667
Yi Shuen Lim <limyishuen@gmail.com>
2019-08-13 15:46:56 -0400

    changes?

--- create-objects.py
+++ create-objects.py
@@ -40,7 +40,6 @@ def instantiate_artist(artist):
 
 
 def secondary_objects(data):
-<<<<<<< HEAD
 
     mechanics = data['mechanics']
     categories = data['categories']
@@ -67,91 +66,14 @@ def secondary_objects(data):
         artists_obj = []
 
     return {'mechanics': mechanics_obj, 'categories': categories_obj, 'artists': artists_obj}
-=======
-	relevant_data = None  # what keys are relevant?
-	# pass thru the checking functions and instantiate
-
-	# checking & instantiating mechanics
-	new_mechanics = list(filter(lambda m: obj_exists(Mechanic, m), names(Mechanic)))
-	if new_mechanics != []:
-		mechanics = [instantiate_mechanic(m) for m in new_mechanics]
-	else:
-		mechanics = []
-	# categories
-	new_categories = list(filter(lambda c: obj_exists(Category, c), names(Category)))
-	if new_categories != []:
-		categories = [instantiate_category(c) for c in new_categories]
-	else:
-		categories = []
-	# artists
-	new_artists = list(filter(lambda a: obj_exists(Artist, a), names(Artist)))
-	if new_artists != []:
-		artists = [instantiate_artist(a) for a in new_artists]
-	else:
-		artists = []
-
-	return {'mechanics': mechanics, 'categories': categories, 'artists': artists}
->>>>>>> e73d7f94fe891c403ddcac4811bfa37c8dfe7755
 
 
 # functions to filter all existing secondary objects for names in a list
 def findmechanics(m):
-<<<<<<< HEAD
-    mechanics = []
-    for i in m:
-        mechanics.extend(session.query(Mechanic).filter(Mechanic.name == i).all())
-    return mechanics
-
-def findcategories(c):
-    categories = []
-    for i in c:
-        categories.extend(session.query(Category).filter(Category.name == i).all())
-    return categories
-
-def findartists(a):
-    artists = []
-    for i in a:
-        artists.extend(session.query(Artist).filter(Artist.name == i).all())
-    return artists
-
-def game_object(data):
-
-    # instantiating secondary objects if they don't yet exist
-    new_objects = secondary_objects(data)
-    for k, v in new_objects:
-        if v != []:
-            session.add_all(v)
-            session.commit()
-
-    game = Game(name=data['name'],
-                description=data['description'],
-                ratingscount=123,
-                avgrating=3.5,
-                published=1999,
-                minplayers=1,
-                maxplayers=10,
-                suggestednumplayers=6,
-                playtime=60,
-                minplaytime=30,
-                maxplaytime=70,
-                minage=4,
-                suggestedage=15,
-                language_dependensce=2,
-                designer='great designer',
-                publisher='best publisher',
-                )
-
-    game.mechanics = findmechanics(data['mechanics'])
-    game.categories = findcategories(data['categories'])
-    game.artists = findartists(data['artists'])
-
-    return game
-=======
 	mechanics = []
 	for i in m:
 		mechanics.extend(session.query(Mechanic).filter(Mechanic.name == i).all())
 	return mechanics
->>>>>>> e73d7f94fe891c403ddcac4811bfa37c8dfe7755
 
 
 def findcategories(c):
