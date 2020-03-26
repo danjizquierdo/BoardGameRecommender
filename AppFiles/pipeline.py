1e16ca87347699d5010afd6fdbb8ef52519086db
Daniel Izquierdo <dan.j.izquierdo@gmail.com>
2020-03-26 15:02:14 -0400

    Comment pipeline script

--- AppFiles/pipeline.py
+++ AppFiles/pipeline.py
@@ -4,13 +4,12 @@ import pandas as pd
 import pickle
 import json
 from operator import itemgetter
-from sklearn.preprocessing import MinMaxScaler, RobustScaler
+from sklearn.preprocessing import MinMaxScaler, QuantileTransformer
 from sklearn.neighbors import NearestNeighbors
 
-# Load data
+# Load raw data
 eng = create_engine('sqlite:///data/boardgames.db', echo=False)
 raw = pd.read_sql_query("SELECT * FROM boardgames", eng).drop(['index', 'designer', 'publisher'], axis=1)
-
 # Open game data
 with open('boardgames.json') as f:
     game_json = json.load(f)
@@ -22,19 +21,23 @@ def preprocess(df):
                 'minplayers', 'maxplayers', 'best', 'recommended', 'not_recommended',
                 'playingtime', 'minplaytime', 'maxplaytime', 'minage', 'suggestedage',
                 'language_dependence']
-
+    # Separate features in terms of appropriate scaling
     outliers = ['published', 'best', 'maxplayers', 'maxplaytime', 'minplaytime', 'not_recommended', 'playingtime',
                 'ratingscount', 'recommended']
     normal = ['language_dependence', 'minage', 'minplayers', 'suggestedage', 'avgrating']
 
-    df[relevant] = df[relevant].apply(lambda x: x.fillna(x.median()) if x.dtype != np.dtype('O') else x,axis=0)
+    # Fill missing values with the median
+    df[relevant] = df[relevant].apply(lambda x: x.fillna(x.median()) if x.dtype != np.dtype('O') else x, axis=0)
 
-    robust = RobustScaler()
-    df[outliers] = robust.fit_transform(df[outliers])
+    # Perform robust scaling by transforming to normal distribution and then within 0-1 range
+    robust = QuantileTransformer()
     minmax = MinMaxScaler()
+    df[outliers] = minmax.fit_transform(robust.fit_transform(df[outliers]))
+    # Perform linear transformation to normally distributed features
     df[normal] = minmax.fit_transform(df[normal])
 
     return df
+
 
 # Store processed data for faster retrieval
 processed = preprocess(raw)
@@ -52,19 +55,21 @@ def get_test_array(names):
     return inputs.mean().values.reshape(1, -1)
 
 
-def filter_mechanics(mechanics, df = processed):
-    ids = []
+def filter_mechanics(mechanics, df=processed):
+    # Filter out games that do not contain the given mechanics
+    ids = set()
     for mechanic in mechanics:
-        ids.extend(df[df[mechanic] == 1].id.values.tolist())
-    return ids
+        ids.add(df[df[mechanic] == 1].id.values.tolist())
+
+    return list(ids)
 
 
+# Get only the relevant column names for feature comparisons
 cols = dropcols(processed).columns.tolist()
 
 
 def feature_closeness(inputs, output):
-    # takes in the names of inputs and also results!
-    
+    # Takes in the names of inputs and results to determine which features were most relevant for recommendations
     invalues = dict(zip(cols, get_test_array(inputs)[0]))
     relevantvals = {k: v for k, v in invalues.items() if v != 0}
     
@@ -77,44 +82,48 @@ def feature_closeness(inputs, output):
 
 
 def store_mvp(data, *filename):
+    # Creates and returns MVP Nearest Neighbors model to use for recommendations
     model = NearestNeighbors(n_neighbors=5)
     model.fit(dropcols(preprocess(data)))
+    # Saves file if a filename was given
     if filename:
         pickle.dump(model, open(f'{filename}.p', 'wb'))
     return model
 
 
 def get_nearest(names, mechanics, n=100):
-
-    # Grab info for given games
+    # Grab info for given games if games were given
     if names:
+        # Set up inputs
         input_array = get_test_array(names)
         # Find the nearest neighbors
         dists, neighbors = nn.kneighbors(input_array, n+len(names))
         dists = dists.tolist()[0]
         neighbors = neighbors.tolist()[0]
-        neighborhood = pd.DataFrame(np.array([dists,neighbors]).T,columns=['distance','id'])
-        # Scale distances by inverse of avgrating
-        weights = processed.query('id == @neighbors')[['id','avgrating']]
+        neighborhood = pd.DataFrame(np.array([dists, neighbors]).T, columns=['distance', 'id'])
+        # Grab avgrating for each game to be used as weighting factor
+        weights = processed.query('id == @neighbors')[['id', 'avgrating']]
+        # If mechanics were given, bias towards games with matching mechanics
         if mechanics:
-            # Prefer games with matching mechanics
+            # Prefer games with matching mechanics by adjusting their avgrating
             mech_games = filter_mechanics(mechanics)
-            weights['avgrating'] = weights.apply(lambda x: x['avgrating']*10 if x['id'] in mech_games else x['avgrating'], axis=1)
-        # Sort results by new scaled distance
-        neighborhood['distance'] = pd.merge(neighborhood, weights,on='id').apply(lambda x: x['distance']/(x['avgrating']+.01),axis=1)
-        neighborhood['id']=neighborhood['id'].apply(int)
-        neighborhood.sort_values('distance',inplace=True)
-        # neighborhood = neighborhood[~neighborhood['names'].isin(names)].sort_values('distance').head(3)
+            weights['avgrating'] = weights.apply(
+                lambda x: x['avgrating']*10 if x['id'] in mech_games else x['avgrating'], axis=1)
+        # Scale distances by inverse of avgrating, bias towards higher rated games. Sort results by new scaled distance
+        neighborhood['distance'] = pd.merge(neighborhood, weights, on='id').apply(
+            lambda x: x['distance']/(x['avgrating']+.01), axis=1)
+        # Make sure id is correct dtype
+        neighborhood['id'] = neighborhood['id'].apply(int)
+        neighborhood.sort_values('distance', inplace=True)
         # Return results not in the given names
         results = []
         for idx in neighborhood['id'].tolist():
             if game_json[idx]['name'] not in names:
                 results.append(game_json[idx])
-            # print(game_json[idx]['distance'])
-        # results = sorted(list(filter(lambda g: g['bggid'] in neighborhood['id'].tolist() and g['name'] not in names, game_json)), key=lambda g: g['distance'])[:3]
-
+        # Find what results were most impactful in the recommendation decision
         for r in results:
             r['bestfeatures'] = feature_closeness(names, r['name'])
+    # If only mechanics were given, sort out games without those mechanics and recommend top games
     elif mechanics:
         # Filters games based on given mechanics
         mech_games = filter_mechanics(mechanics)
@@ -122,6 +131,7 @@ def get_nearest(names, mechanics, n=100):
         if mech_games:
             best_mech = processed.query('id == @mech_games').sort_values('avgrating',
                                                                          ascending=False)['id'].head(3).values
+        # If there are no games with all of the given mechanics, go through each individually
         else:
             print('No mech_games')
             best_mech = processed.sort_values('avgrating',ascending=False)['id'].head(3).values
